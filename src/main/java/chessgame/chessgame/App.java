e34b21739ada5c53fc7cca696faf9f13403fa2df
Jérémy Huet <huetjeremy@outlook.com>
2022-08-27 00:04:55 +0200

    Project creation

---
+++ src/main/java/chessgame/chessgame/App.java
@@ -0,0 +1,8 @@
+package chessgame.chessgame;
+
+public class App {
+
+	public static void main(String[] args) {
+		AppFx.start(args);
+	}
+}
---
+++ src/main/java/chessgame/chessgame/SecondaryController.java
@@ -0,0 +1,13 @@
+package chessgame.chessgame;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        AppFx.setRoot("primary");
+    }
+}
