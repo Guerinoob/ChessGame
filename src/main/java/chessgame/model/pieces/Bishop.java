2dabfa3b77e2aef6c13a4d4d39cbb582e3c9a97c
Jérémy Huet <huetjeremy@outlook.com>
2022-09-01 19:22:07 +0200

    Implemented bishop moves

--- src/main/java/chessgame/model/pieces/Bishop.java
+++ src/main/java/chessgame/model/pieces/Bishop.java
@@ -1,7 +1,9 @@
 package chessgame.model.pieces;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import chessgame.model.board.Board;
 import chessgame.model.board.Cell;
 import chessgame.model.player.Color;
 
@@ -13,8 +15,77 @@ public class Bishop extends Piece {
 
 	@Override
 	public List<Cell> getPossibleMoves() {
-		// TODO Auto-generated method stub
-		return null;
+		final var currentRow = cell.getRow();
+		final var currentColumn = cell.getColumn();
+		final var board = cell.getBoard();
+				
+		final var moves = new ArrayList<Cell>();
+		
+		for(int i = 1; i < Board.LENGTH; i++) {
+			final var dest = board.getCell(currentRow - i, currentColumn - i);
+			
+			if(checkCell(dest) == false)
+				break;
+			
+			moves.add(dest);
+			
+			if(isOccupied(dest))
+				break;
+		}
+		
+		for(int i = 1; i < Board.LENGTH; i++) {
+			final var dest = board.getCell(currentRow - i, currentColumn + i);
+						
+			if(checkCell(dest) == false)
+				break;
+			
+			moves.add(dest);
+			
+			if(isOccupied(dest))
+				break;
+		}
+		
+		for(int i = 1; i < Board.LENGTH; i++) {
+			final var dest = board.getCell(currentRow + i, currentColumn - i);
+			
+			if(checkCell(dest) == false)
+				break;
+			
+			moves.add(dest);
+			
+			if(isOccupied(dest))
+				break;
+		}
+		
+		for(int i = 1; i < Board.LENGTH; i++) {
+			final var dest = board.getCell(currentRow + i, currentColumn + i);
+			
+			if(checkCell(dest) == false)
+				break;
+			
+			moves.add(dest);
+			
+			if(isOccupied(dest))
+				break;
+		}
+		
+		return moves;
+	}
+	
+	private boolean checkCell(Cell cell) {
+		if(cell == null)
+			return false;
+		
+		final var piece = cell.getPiece();
+		
+		if(piece != null && piece.getColor().equals(color))
+			return false;
+		
+		return true;
+	}
+	
+	private boolean isOccupied(Cell cell) {
+		return cell.getPiece() != null;
 	}
 
 }
