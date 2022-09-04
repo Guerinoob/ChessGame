361b49e1b3bc2babc8c5bb43b699b89ee2284663
Jérémy Huet <huetjeremy@outlook.com>
2022-09-04 16:57:16 +0200

    Use of anonymous classes for directional moves

--- src/main/java/chessgame/model/pieces/Bishop.java
+++ src/main/java/chessgame/model/pieces/Bishop.java
@@ -3,7 +3,9 @@ package chessgame.model.pieces;
 import java.util.List;
 
 import chessgame.model.board.Cell;
-import chessgame.model.movements.BishopDirectionStrategy;
+import chessgame.model.direction.Direction;
+import chessgame.model.direction.EDirection;
+import chessgame.model.movements.DirectionalStrategy;
 import chessgame.model.player.Color;
 
 public class Bishop extends Piece {
@@ -14,7 +16,19 @@ public class Bishop extends Piece {
 
 	@Override
 	public List<Cell> getPossibleMoves() {				
-		return new BishopDirectionStrategy(this).getPossibleMoves();
+		return new DirectionalStrategy(this) {
+			
+			@Override
+			public List<Cell> getPossibleMoves() {
+				this.addDirections(List.of(
+					new Direction(EDirection.UP_LEFT),
+					new Direction(EDirection.UP_RIGHT),
+					new Direction(EDirection.BOTTOM_LEFT),
+					new Direction(EDirection.BOTTOM_RIGHT)
+				));
+				return super.getPossibleMoves();
+			}
+		}.getPossibleMoves();
 	}
 
 }
--- src/main/java/chessgame/model/pieces/Rook.java
+++ src/main/java/chessgame/model/pieces/Rook.java
@@ -3,7 +3,9 @@ package chessgame.model.pieces;
 import java.util.List;
 
 import chessgame.model.board.Cell;
-import chessgame.model.movements.RookDirectionalStrategy;
+import chessgame.model.direction.Direction;
+import chessgame.model.direction.EDirection;
+import chessgame.model.movements.DirectionalStrategy;
 import chessgame.model.player.Color;
 
 public class Rook extends Piece {
@@ -14,7 +16,19 @@ public class Rook extends Piece {
 
 	@Override
 	public List<Cell> getPossibleMoves() {
-		return new RookDirectionalStrategy(this).getPossibleMoves();
+		return new DirectionalStrategy(this) {
+			
+			@Override
+			public List<Cell> getPossibleMoves() {
+				this.addDirections(List.of(
+					new Direction(EDirection.UP),
+					new Direction(EDirection.BOTTOM),
+					new Direction(EDirection.LEFT),
+					new Direction(EDirection.RIGHT)
+				));
+				return super.getPossibleMoves();
+			}
+		}.getPossibleMoves();
 	}
 
 }
